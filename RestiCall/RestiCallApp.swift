874383bd17b94fa1dd6d5e25cca70398819f0a3b
msuleman0100 <msuleman0100@gmail.com>
2023-04-27 08:08:51 +0500

    project added

---
+++ RestiCall/RestiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestiCallApp.swift
+//  RestiCall
+//
+//  Created by Muhammad Suleman on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RestiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
